e4da6bbc7e65c9a45c2549585fdfb01693c53b4b
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-04-11 09:30:45 +0200

    texture mapping

--- c_files/image.c
+++ c_files/image.c
@@ -12,26 +12,45 @@
 
 #include "cub3d.h"
 
-void	ft_mymlx_pixelput(t_data *data, int skycolor, int groundcolor)
+void	ft_mymlx_pixelput(t_all *vars, t_data *data)
 {
 	char	*dst;
+	int	color;
 	int	x;
 	int	y;
+	int	px;
+	int	py;
 
+
+	printf("data->ll = %d, data->bpp / 8 = %d \n", data->ll, data->bpp / 8);
 	x = 0;
-	y = 0;
-	while (x < 640)
+	while (x < vars->rsl.w)
 	{
+		printf("(y - 20) * data->ll = %d \n", (y - 20) * data->ll);
 		y = 0;
-		while (y < 480)
+		while (y < vars->rsl.h)
 		{
-			dst = data->addr + (y * data->ll + x * (data->bpp / 8));
-			if (y < 480 / 2)
-				*(unsigned int*)dst = skycolor;
-			else
-				*(unsigned int*)dst = groundcolor;
+			dst = data->addr + (y * data->ll + x * 
+					(data->bpp / 8));
+			px = x / (vars->rsl.w / 32) % 64;
+			py = y / (vars->rsl.h / 32) % 64;
+			color =	ft_get_xpm_pixel(&vars->spr.data[3], 
+					px, py);
+			if (color == 0)
+				printf("y / rslh / 32 = %d \n", y / (vars->rsl.h / 32));
+			*(unsigned int*)dst = color;
 			y++;
 		}
 		x++;
 	}
+}
+
+int	ft_get_xpm_pixel(t_data *data, int x, int y)
+{
+	int	color;
+
+	/*if (y >= 32)
+		y = 31;*/
+	color = *((int*)data->addr + (x * 2 + y * 128));
+	return (color);
 }
