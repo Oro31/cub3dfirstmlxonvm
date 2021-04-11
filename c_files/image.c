17b1865cd0ce0edda8670a6a639084127bca5596
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-04-11 09:39:02 +0200

    texture mapping OK !!!!!

--- c_files/image.c
+++ c_files/image.c
@@ -22,22 +22,20 @@ void	ft_mymlx_pixelput(t_all *vars, t_data *data)
 	int	py;
 
 
-	printf("data->ll = %d, data->bpp / 8 = %d \n", data->ll, data->bpp / 8);
 	x = 0;
 	while (x < vars->rsl.w)
 	{
-		printf("(y - 20) * data->ll = %d \n", (y - 20) * data->ll);
 		y = 0;
 		while (y < vars->rsl.h)
 		{
 			dst = data->addr + (y * data->ll + x * 
 					(data->bpp / 8));
-			px = x / (vars->rsl.w / 32) % 64;
-			py = y / (vars->rsl.h / 32) % 64;
+			px = (x / (vars->rsl.w / 64)) % 64;
+			py = (y / (vars->rsl.h / 64)) % 64;
 			color =	ft_get_xpm_pixel(&vars->spr.data[3], 
 					px, py);
 			if (color == 0)
-				printf("y / rslh / 32 = %d \n", y / (vars->rsl.h / 32));
+				printf("px = %d, py = %d \n", px, py);
 			*(unsigned int*)dst = color;
 			y++;
 		}
@@ -49,8 +47,6 @@ int	ft_get_xpm_pixel(t_data *data, int x, int y)
 {
 	int	color;
 
-	/*if (y >= 32)
-		y = 31;*/
-	color = *((int*)data->addr + (x * 2 + y * 128));
+	color = *((int*)data->addr + (x + y * 64));
 	return (color);
 }
